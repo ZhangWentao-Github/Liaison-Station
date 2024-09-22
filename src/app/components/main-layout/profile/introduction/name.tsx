9168a5a6d09c9ba05d8f1f06df81e2ff1df948b8
Zhang Wentao <mailbox@zhang-wen-tao.com>
2024-09-22 23:52:04 +0800

    Github

--- src/app/components/main-layout/profile/introduction/name.tsx
+++ src/app/components/main-layout/profile/introduction/name.tsx
@@ -11,7 +11,7 @@ type INameProps = BasicsProps & MotionProps;
 export const Name: React.FC<INameProps> = ({ className, style, ...rest }) => (
   <motion.div className={cn('flex h-12 flex-col justify-center gap-2', className)} style={style} {...rest}>
     <h1 className="my-0 px-4 font-kanit text-3xl font-light text-white/90">
-      Hi, I&apos;m <span className="font- font-normal text-white/90">LF112</span>
+      Hi, I&apos;m <span className="font- font-normal text-white/90">张文涛</span>
     </h1>
     <Separator className="h-[3px] w-full rounded bg-profile-line shadow" />
   </motion.div>
